505070f4342c9422121a9594b3d7a08455c014e6
Andrey <andreys2009@gmail.com>
2016-03-15 15:54:08 -0600

    Css forma parte 2

--- sites/all/themes/asobitico/js/script.js
+++ sites/all/themes/asobitico/js/script.js
@@ -22,89 +22,41 @@
 		}
 
 
+		/* Script efecto scroll a las anclas */
+		$('a[href^="#"]').on('click',function (e) {
+			e.preventDefault();
+			var target = this.hash,
+			$target = $(target);
+		
+			$('html, body').stop().animate({
+				'scrollTop': $target.offset().top
+			}, 900, 'swing', function () {
+				window.location.hash = target;
+			});
+		});
 
+		$('.menu-anclas li a').on('click', function(){
+			$('.menu-anclas li a').removeClass("activ");
+			$(this).addClass("activ");
+		});
 
 
 		/*$(window).scroll(function(){
-			    scrolls = $(window).scrollTop();
-
-			    if($(".vista-impacto-regional-detalle").length > 0){
-				    var ini = $(".impacto-regional-detalle-imagen-top").position();
-				    ini = ini.top;
-				    var fin = $("#empleos-disponibles").position();
-				    fin = fin.top;
-				    if (scrolls > ini && scrolls < (fin+100)) { 
-				       $(".submenu-paises").addClass('opensub');
-				    } else {
-				       $(".submenu-paises").removeClass('opensub');
-				    }
-
-				    if($("#datos-numericos-pais").length >0 ){
-				    	var sub1 = $("#datos-numericos-pais").position();
-				    	sub1 = sub1.top;
-				    }
-				    if($("#impacto-positivo-pais").length >0 ){
-				    	var sub2 = $("#impacto-positivo-pais").position();
-				    	sub2 = sub2.top;
-					}
-
-				    if($("#testimonios-pais-detalle").length >0 ){
-				    	var sub3 = $("#testimonios-pais-detalle").position();
-				    	sub3 = sub3.top;
-					}
-
-					if($("#empleos-disponibles").length >0 ){
-				    	var sub4 = $("#empleos-disponibles").position();
-				    	sub4 = sub4.top;
-				    }    	    
-
-				    if(scrolls >= sub1){
-				    	$(".subm").removeClass('activ');
-				    	$(".opt1").addClass('activ');
-				    }
-				    if(scrolls >= sub2){
-				    	$(".subm").removeClass('activ');
-				    	$(".opt2").addClass('activ');
-				    }
-				    if(scrolls >= sub3){
-				    	$(".subm").removeClass('activ');
-				    	$(".opt3").addClass('activ');
-				    }	    	    
-				    if(scrolls >= sub4){
-				    	$(".subm").removeClass('activ');
-				    	$(".opt4").addClass('activ');
-				    }
-				}
-				if($(".vista-user-micuenta").length > 0){
-
-				    var ini = $("#imagen-nombre").position();
-				    ini = ini.top;
-				    var fin = $("#boton-enviar").position();
-				    fin = fin.top;
-				    if (scrolls > (ini-300) && scrolls < (fin-200)) { 
-				       $(".submenu-lateral").addClass('opensub');
-				    } else {
-				       $(".submenu-lateral").removeClass('opensub');
-				    }
-
-				    var sub1 = $("#puestos-aplicados").position();
-				    sub1 = sub1.top;
-				    var sub2 = $("#editar-perfil").position();
-				    sub2 = sub2.top;
-
-				    if(scrolls >= sub1){
-				    	$(".subm").removeClass('activ');
-				    	$(".opt1").addClass('activ');
-				    }
-				    if(scrolls >= sub2){
-				    	$(".subm").removeClass('activ');
-				    	$(".opt2").addClass('activ');
-				    }			
-				}
-
-			});*/
-
-
+		  scrolls = $(window).scrollTop();
+			var scrollPos = $(document).scrollTop();
+	    $('.menu-anclas li a').each(function (indice, elemento) { console.log(indice);
+	        var currlink = $(this).find('a'); console.log(currlink.attr("href"));
+	        var refelement = $(currlink.attr("href"));
+	        refelement = refelement.offset();
+	        if (refelement.top <= scrollPos) {
+	            $('.menu-anclas ul li a').removeClass("activ");
+	            currlink.addClass("activ");
+	        }
+	        else{
+	            currlink.removeClass("activ");
+	        }
+	    });
+		});*/
 
 
 	});
